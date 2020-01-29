b2ec1965b36c0a38beeac5cc6c1ff48cf402a69b
Lee Garrett <lgarrett@rocketjump.eu>
2020-01-29 16:33:20 +0100

    Example usage of python modules

---
+++ letsplay.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from banana_hammock.strings import make_small, make_string_big, make_reverse
+
+user_input = input("Please enter some text: ")
+
+
+print('Making it small: ', make_small(user_input))
