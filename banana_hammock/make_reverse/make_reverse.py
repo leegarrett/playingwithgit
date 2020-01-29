e5e7e5b1b1021b84b938dfccfe3165d1c076f768
Karim Hamdi <git@karimhamdi.de>
2020-01-29 14:26:37 +0100

    removed debug from make_reverse

--- banana_hammock/make_reverse/make_reverse.py
+++ banana_hammock/make_reverse/make_reverse.py
@@ -1,8 +1,5 @@
-def make_reverse (my_string:str='Hello World'):
+def make_reverse (my_string:str='Hello 	world'):
 	a=''
-	print(range(len(my_string)-1,0,-1))
 	for i in range(len(my_string),0,-1):
 		a+=my_string[i-1]
-	return a
-
-print(make_reverse('Saipoacoapias'))
+	return a	
