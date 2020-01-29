fbd6df4862da51222de3c77b34449aa0f3cdf84b
Lee Garrett <lgarrett@rocketjump.eu>
2020-01-29 14:45:54 +0100

    Add docstring and function annotations

--- banana_hammock/make_small/make_small.py
+++ banana_hammock/make_small/make_small.py
@@ -1,2 +1,6 @@
-def make_small (mystring):
+def make_small (mystring: str) -> str:
+    """pass a string as only parameter
+
+    Returns the lower-cased string
+    """
     return mystring.lower()
